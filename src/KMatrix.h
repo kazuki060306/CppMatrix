6f3d018c0eddfdc14c80ad11030942f1a5d6b930
Foxgear_KM <80465569+kazuki060306@users.noreply.github.com>
2023-08-11 10:55:28 +0900

    機能拡張

--- src/KMatrix.h
+++ src/KMatrix.h
@@ -1,4 +1,8 @@
+﻿#ifndef KMATRIX_H
+#define KMATRIX_H
 
+#include <algorithm>
+#include <iterator>
 
 /*
  * 行列演算クラス
@@ -9,121 +13,376 @@ class KMatrix
 {
 private:
 
-	int m_Rows;			// 行数
-	int m_Cols;			// 列数
-	arraytype* m_data;	// 配列データ
+	unsigned int m_Rows;	// 行数
+	unsigned int m_Cols;	// 列数
+	unsigned int m_Size;	// 配列サイズ
+	arraytype* m_Data;		// 配列データ
 
 public:
+
+	enum AXIS_KIND
+	{
+		ROW = 0,
+		COL,
+		MAX
+	};
 
 	/*
 	 * コンストラクタ
 	 */
-	KMatrix(int row, int col)
+	KMatrix()
+		: m_Rows()
+		, m_Cols()
+		, m_Size()
+		, m_Data()
+	{
+	}
+
+	/*
+	 * コンストラクタ
+	 */
+	KMatrix(unsigned int row, unsigned int col)
 		: m_Rows(row)
 		, m_Cols(col)
+		, m_Size(row * col)
 	{
-		m_data = new arraytype[row * col];
+		m_Data = new arraytype[m_Size];
+		memset(m_Data, 0, sizeof(arraytype) * m_Size);
 	}
 	 
-	 
-	 /*
-	 * 行数の取得 
+	/*
+	 * デストラクタ
 	 */
-	const int row()
+	~KMatrix()
+	{
+		if (m_Data != nullptr)
+		{
+			//delete[] m_Data;
+		}
+	}
+
+	/*
+	 * 行数を取得
+	 */
+	const unsigned int size() const
+	{
+		return this->m_Size;
+	}
+
+	/*
+	 * 行数を取得 
+	 */
+	const unsigned int row() const
 	{
 		return this->m_Rows;
 	}
 
 	/*
-	 * 列数の取得
+	 * 列数を取得
 	 */
-	const int col()
+	const unsigned int col() const
 	{
 		return this->m_Cols;
 	}
 
 	/*
-	 * 配列データの取得
+	 * 配列データを取得
 	 */
-	arraytype* data()
+	arraytype* data() const
 	{
-		return this->m_data;
+		return this->m_Data;
 	}
 
 	/*
-	 * 転置行列の取得
+	 * 指定行のデータを取得
 	 */
-	KMatrix<arraytype>& transpose()
+	KMatrix<arraytype> rowdata(unsigned int row) const
+	{
+
+		KMatrix<arraytype> rowdata(1, this->m_Cols);
+		memcpy(rowdata.m_Data, this->m_Data + (row * this->m_Cols), sizeof(arraytype) * this->m_Cols);
+		return rowdata;
+	}
+
+	/*
+	 * 指定列のデータを取得
+	 */
+	KMatrix<arraytype> coldata(unsigned int col) const
+	{
+		KMatrix<arraytype> coldata(this->m_Rows, 1);
+		memcpy(coldata.m_Data, this->transpose().m_Data + (col * this->m_Rows), sizeof(arraytype) * this->m_Rows);
+		return coldata;
+	}
+
+	/*
+	 * 配列データの最大値を取得
+	 */
+	arraytype amax() const
+	{
+		// 1次元のみ対応
+		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::amax error : data is not 1 demension.");
+		}
+
+		arraytype maxval = this->m_Data[0];
+		for (unsigned int i = 1; i < this->m_Size; ++i)
+		{
+			if (maxval < this->m_Data[i])
+			{
+				maxval = this->m_Data[i];
+			}
+		}
+		return maxval;
+	}
+
+	/*
+	 * 配列データの最小値を取得
+	 */
+	arraytype amin() const
+	{
+		// 1次元のみ対応
+		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::diag error : data is not 1 demension.");
+		}
+
+		arraytype minval = this->m_Data[0];
+		for (unsigned int i = 1; i < this->m_Size; ++i)
+		{
+			if (minval > this->m_Data[i])
+			{
+				minval = this->m_Data[i];
+			}
+		}
+		return minval;
+	}
+
+	/*
+	 * 配列データの最大値のindexを取得
+	 */
+	unsigned int argmax() const
+	{
+		// 1次元のみ対応
+		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::diag error : data is not 1 demension.");
+		}
+		arraytype maxval = this->m_Data[0];
+		unsigned int maxelement = 0;
+		for (unsigned int i = 1; i < this->m_Size; ++i)
+		{
+			if (maxval < this->m_Data[i])
+			{
+				maxelement = i;
+			}
+		}
+		return maxelement;
+	}
+
+	/*
+	 * 配列データの最小値のindexを取得
+	 */
+	unsigned int argmin() const
+	{
+		// 1次元のみ対応
+		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::diag error : data is not 1 demension.");
+		}
+
+		arraytype minval = this->m_Data[0];
+		unsigned int minelement = 0;
+		for (unsigned int i = 1; i < this->m_Size; ++i)
+		{
+			if (minval > this->m_Data[i])
+			{
+				minelement = i;
+			}
+		}
+		return minelement;
+	}
+
+	/*
+	 * 転置行列を取得
+	 */
+	KMatrix<arraytype> transpose() const
 	{
 		KMatrix<arraytype> transmat(m_Cols, m_Rows);
-		for (int i = 0; i < m_Rows; ++i)
+
+		// 1次元の場合
+		if (this->m_Rows != 1 && this->m_Cols != 1)
 		{
-			for (int j = 0; j < m_Cols; ++j)
+			transmat.m_Cols = this->m_Rows;
+			transmat.m_Rows = this->m_Cols;
+			transmat.m_Data = this->m_Data;
+		}
+		else
+		{
+			for (unsigned int i = 0; i < this->m_Rows; ++i)
 			{
-				transmat(j, i) = operator()(i, j);
+				for (unsigned int j = 0; j < this->m_Cols; ++j)
+				{
+					transmat(j, i) = operator()(i, j);
+				}
 			}
 		}
 		return transmat;
 	}
 
 	/*
-	 * ゼロベクトルの取得
+	 * ゼロベクトルを取得
 	 */
-	KMatrix<arraytype>& zeros(int row, int col)
+	void zeros()
 	{
-		KMatrix<arraytype> zaromat(row, col);
-		for (int i = 0; i < row; ++i)
-		{
-			for (int j = 0; j < col; ++j)
-			{
-				zaromat(j, i) = arraytype(0);
-			}
-		}
-		return zaromat;
+		memset(this->m_Data, 0, sizeof(arraytype) * this->m_Size);
 	}
 
 	/*
-	 * 対角行列の取得
+	 * サイズの確保
 	 */
-	KMatrix<arraytype>& diag()
+	void reserve(unsigned int row, unsigned int col)
 	{
-		if (m_Rows != 1)
+		if (this->m_Data != nullptr)
 		{
-			// 1次元のみ対応
+			delete[] this->m_Data;
+		}
+		this->m_Rows = row;
+		this->m_Cols = col;
+		this->m_Size = row * col;
+		this->m_Data = new arraytype[m_Size];
+		memset(m_Data, 0, sizeof(arraytype) * this->m_Size);
+	}
+
+	/*
+	 * 行・列の変更
+	 */
+	void resize(unsigned int row, unsigned int col)
+	{
+		// 変更不可
+		if (this->m_Size != row * col)
+		{
+			throw std::runtime_error("KMatrix::resize error : data size is not same arg (row * col).");
+		}
+		this->m_Rows = row;
+		this->m_Cols = col;
+	}
+
+	/*
+	 * 対角行列を取得
+	 */
+	KMatrix<arraytype>& diag() const
+	{
+		// 1次元のみ対応
+ 		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::diag error : data is not 1 demension.");
 		}
 
-		KMatrix<arraytype> diagmat = zeros(m_Cols, m_Cols);
-		for (int i = 0; i < m_Cols; ++i)
+		// 対角上に配列を詰める
+		KMatrix<arraytype> diagmat(this->m_Size, this->m_Size);
+		for (unsigned int i = 0; i < this->m_Size; ++i)
 		{
-			diagmat(i, i) = operator()(0, i);
+			diagmat(i, i) = this->m_Data[i];
 		}
 		return diagmat;
 	}
 
 	/*
+	 * 全要素1の行列を取得
+	 */
+	void ones()
+	{
+		// 対角上に配列を詰める
+		for (unsigned int i = 0; i < this->m_Size; ++i)
+		{
+			this->m_Data[i] = arraytype(1);
+		}
+	}
+
+	/*
+	 * 行列の絶対値を取得
+	 */
+	KMatrix<arraytype>& abs() const
+	{
+		KMatrix<arraytype> absmat(this->m_Rows, this->m_Cols);
+		memcpy(absmat.m_Data, this->m_Data, sizeof(arraytype) * this->m_Size);
+		for (unsigned int i = 0; i < this->m_Size; ++i)
+		{
+			if (0 > absmat.m_Data[i])
+			{
+				absmat.m_Data[i] = std::abs(absmat.m_Data[i]);
+			}
+		}
+		return absmat;
+	}
+
+	/*
+	 * 要素毎の積を取得
+	 */
+	KMatrix<arraytype>& multiply(const KMatrix<arraytype>& b) const
+	{
+		// 計算不可
+		if (this->m_Cols != b.m_Cols || this->m_Rows != b.m_Rows)
+		{
+			throw std::runtime_error("KMatrix::operator* error ; size is not same.");
+		}
+
+		KMatrix<arraytype> multiply(this->m_Rows, this->m_Cols);
+		for (unsigned int i = 0; i < this->m_Size; ++i)
+		{
+			multiply.m_Data[i] = this->m_Data[i] * b.m_Data[i];
+		}
+
+		return multiply;
+	}
+
+	/*
+	 * 平均値を取得
+	 */
+	arraytype mean() const
+	{
+		// 1次元のみ対応
+		if (this->m_Rows != 1 && this->m_Cols != 1)
+		{
+			throw std::runtime_error("KMatrix::diag error : data is not 1 demension.");
+		}
+
+		arraytype meanval = 0;
+		for (unsigned int i = 0; i < this->m_Size; ++i)
+		{
+			meanval += this->m_Data[i];
+		}
+
+		return meanval /= this->m_Size;
+	}
+
+	/*
 	 * ()演算子:行列アクセス
 	 */
-	arraytype& operator()(int row, int col)
+	arraytype& operator()(unsigned int row, unsigned int col) const
 	{
-		return this->m_data[row * m_Cols + col];
+		return this->m_Data[row * this->m_Cols + col];
 	}
 
 	/*
 	 * *演算子:行列積
 	 */
-	KMatrix<arraytype>& operator*(const KMatrix<arraytype>& b)
+	KMatrix<arraytype>& operator*(const KMatrix<arraytype>& b) const
 	{
-		if (m_Rows != b.m_Cols)
+		// 計算不可
+		if (this->m_Cols != b.m_Rows)
 		{
-			// 計算不可
+			throw std::runtime_error("KMatrix::operator* error ; cols is not same row.");
 		}
 
-		KMatrix<arraytype> matmul(m_Rows, b.m_Cols);
-		for (int k = 0; k < m_Rows; ++k)
+		// 行列積計算
+		KMatrix<arraytype> matmul(this->m_Rows, b.m_Cols);
+		for (unsigned int k = 0; k < this->m_Rows; ++k)
 		{
-			for (int i = 0; i < b.m_Cols; ++i)
+			for (unsigned int i = 0; i < b.m_Cols; ++i)
 			{
-				for (int j = 0; j < b.m_Rows; ++j)
+				for (unsigned int j = 0; j < b.m_Rows; ++j)
 				{
 					matmul(k, i) += operator()(k, j) * b(j, i);
 				}
@@ -138,9 +397,11 @@ public:
 	 */
 	KMatrix<arraytype>& operator=(const KMatrix<arraytype>& b)
 	{
-		m_Rows = b.m_Rows;
-		m_Cols = b.m_Cols;
-		m_data = b.m_data;
+		this->m_Rows = b.m_Rows;
+		this->m_Cols = b.m_Cols;
+		this->m_Size = b.m_Size;
+		memcpy(this->m_Data, b.m_Data, sizeof(arraytype) * this->m_Size);
 	}
 
 };
+#endif	// KMATRIX_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,47 +1,73 @@
-#define NUMCPP_NO_USE_BOOST
+﻿#define NUMCPP_NO_USE_BOOST
+#define VALUE_CHECK
 
 #include <NumCpp.hpp>
 #include <Eigen>
-//#include <numeric/ublas/vector.hpp>
-//#include <numeric/ublas/matrix.hpp>
 #include "KMatrix.h"
 
 int main()
 {
-	// �s�� (1 2 3)
-	// �@�@ (2 4 6)
-	// �@�@ (3 6 9)
+	// 行列 (1 2 3)
+	// 　　 (2 4 6)
+	// 　　 (3 6 9)
 
-	//clock_t start = clock();    // �X�^�[�g����
+	//clock_t start = clock();    // スタート時間
 	//boost::numeric::ublas::matrix<int> a(150, 10);
 	//for (int i = 0; i < a.size1() ; ++i)
 	//	for (int j = 0; j < a.size2(); ++j)
 	//		a(i, j) = (i + 1) * (j + 1);
 	//boost::numeric::ublas::matrix<int> a_ = prod(a, trans(a));
-	//clock_t end = clock();     // �I������
+	//clock_t end = clock();     // 終了時間
 	//std::cout << "duration = " << (double)(end - start) / CLOCKS_PER_SEC << "sec.\n";
+	std::chrono::system_clock::time_point  start, end;
 
-	clock_t start = clock();    // �X�^�[�g����
-	nc::NdArray<int> b(150, 10);
-	for (int i = 0; i < b.numRows(); ++i)
-	{
-		for (int j = 0; j < b.numCols(); ++j)
-		{
-			b(i, j) = (i + 1) * (j + 1);
-		}
-	}
-	nc::NdArray<int> b_ = matmul(b, b.transpose());
-	clock_t end = clock();     // �I������
-	std::cout << "duration = " << (double)(end - start) / CLOCKS_PER_SEC << "sec.\n";
+	// =======================================================================================
+	// NumCpp
+	// =======================================================================================
+	//start = std::chrono::system_clock::now(); // 計測開始時間
+	//nc::NdArray<int> b(150, 10);
+	//for (int i = 0; i < b.numRows(); ++i)
+	//{
+	//	for (int j = 0; j < b.numCols(); ++j)
+	//	{
+	//		b(i, j) = (i + 1) * (j + 1);
+	//	}
+	//}
+	//nc::NdArray<int> b_ = matmul(b, b.transpose());
+	//end = std::chrono::system_clock::now();  // 計測終了時間
+	//double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	//std::cout << "duration = " << elapsed << "nanosec.\n";
 
-	start = clock();    // �X�^�[�g����
-	Eigen::MatrixXd c(150, 10);
+	// =======================================================================================
+	// Eigen
+	// =======================================================================================
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	Eigen::MatrixXd c(5, 10);
 	for (int i = 0; i < c.rows(); ++i)
 		for (int j = 0; j < c.cols(); ++j)
 			c(i, j) = (i + 1) * (j + 1);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	double elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "Eigen::constructor" << std::endl;
+	for (int i = 0; i < c.rows(); ++i)
+	{
+		for (int j = 0; j < c.cols(); ++j)
+		{
+			std::cout << c(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
 	Eigen::MatrixXd c_ = c * c.transpose();
-	end = clock();     // �I������
-	std::cout << "duration = " << (double)(end - start) / CLOCKS_PER_SEC << "sec.\n";
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "Eigen::matmul" << std::endl;
 	for (int i = 0; i < c_.rows(); ++i)
 	{
 		for (int j = 0; j < c_.cols(); ++j)
@@ -50,9 +76,13 @@ int main()
 		}
 		std::cout << std::endl;
 	}
+#endif
 
-	start = clock();    // �X�^�[�g����
-	KMatrix<int> a(150, 10);
+	// =======================================================================================
+	// KMatrix
+	// =======================================================================================
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a(5, 10);
 	for (int i = 0; i < a.row(); ++i)
 	{
 		for (int j = 0; j < a.col(); ++j)
@@ -60,9 +90,28 @@ int main()
 			a(i, j) = (i + 1) * (j + 1);
 		}
 	}
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::constructor" << std::endl;
+	for (int i = 0; i < a.row(); ++i)
+	{
+		for (int j = 0; j < a.col(); ++j)
+		{
+			std::cout << a(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
 	KMatrix<int> a_ = a * a.transpose();
-	end = clock();     // �I������
-	std::cout << "duration = " << (double)(end - start) / CLOCKS_PER_SEC << "sec.\n";
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::matmul" << std::endl;
 	for (int i = 0; i < a_.row(); ++i)
 	{
 		for (int j = 0; j < a_.col(); ++j)
@@ -71,6 +120,253 @@ int main()
 		}
 		std::cout << std::endl;
 	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_multiply = a.multiply(a);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::multiply" << std::endl;
+	for (int i = 0; i < a_multiply.row(); ++i)
+	{
+		for (int j = 0; j < a_multiply.col(); ++j)
+		{
+			std::cout << a_multiply(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_transpose = a_.transpose();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::transpose" << std::endl;
+	for (int i = 0; i < a_transpose.row(); ++i)
+	{
+		for (int j = 0; j < a_transpose.col(); ++j)
+		{
+			std::cout << a_transpose(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_rowdata = a_.rowdata(1);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::rowdata" << std::endl;
+	for (int i = 0; i < a_rowdata.row(); ++i)
+	{
+		for (int j = 0; j < a_rowdata.col(); ++j)
+		{
+			std::cout << a_rowdata(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_coldata = a_.coldata(2);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::coldata" << std::endl;
+	for (int i = 0; i < a_coldata.row(); ++i)
+	{
+		for (int j = 0; j < a_coldata.col(); ++j)
+		{
+			std::cout << a_coldata(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	int a_amax = a_.rowdata(2).amax();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::amax" << std::endl;
+	std::cout << a_amax << std::endl;
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	int a_amin = a_.rowdata(2).amin();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::amin" << std::endl;
+	std::cout << a_amin << std::endl;
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	int a_argmax = a_.coldata(1).argmax();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::argmax" << std::endl;
+	std::cout << a_argmax << std::endl;
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	int a_argmin = a_.coldata(1).argmin();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::argmin" << std::endl;
+	std::cout << a_argmin << std::endl;
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	int a_argmean = a_.coldata(2).mean();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::mean" << std::endl;
+	std::cout << a_argmean << std::endl;
+#endif
+
+	KMatrix<int> a2(5, 10);
+	for (int i = 0; i < a2.row(); ++i)
+	{
+		for (int j = 0; j < a2.col(); ++j)
+		{
+			a2(i, j) = std::pow(-1 * i, j);
+		}
+	}
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::a2" << std::endl;
+	for (int i = 0; i < a2.row(); ++i)
+	{
+		for (int j = 0; j < a2.col(); ++j)
+		{
+			std::cout <<
+				a2(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_abs = a2.abs();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::abs" << std::endl;
+	for (int i = 0; i < a_abs.row(); ++i)
+	{
+		for (int j = 0; j < a_abs.col(); ++j)
+		{
+			std::cout <<
+				a_abs(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_rowdiag = a_.rowdata(2).diag();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::diag" << std::endl;
+	for (int i = 0; i < a_rowdiag.row(); ++i)
+	{
+		for (int j = 0; j < a_rowdiag.col(); ++j)
+		{
+			std::cout <<
+				a_rowdiag(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_coldiag = a_.coldata(1).diag();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::diag" << std::endl;
+	for (int i = 0; i < a_coldiag.row(); ++i)
+	{
+		for (int j = 0; j < a_coldiag.col(); ++j)
+		{
+			std::cout <<
+				a_coldiag(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	KMatrix<int> a_ones(5, 7);
+	a_ones.ones();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::ones" << std::endl;
+	for (int i = 0; i < a_ones.row(); ++i)
+	{
+		for (int j = 0; j < a_ones.col(); ++j)
+		{
+			std::cout <<
+				a_ones(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	a.zeros();
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::zeros" << std::endl;
+	for (int i = 0; i < a.row(); ++i)
+	{
+		for (int j = 0; j < a.col(); ++j)
+		{
+			std::cout << 
+				a(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
+
+	start = std::chrono::system_clock::now(); // 計測開始時間
+	a_.reserve(3, 5);
+	end = std::chrono::system_clock::now();  // 計測終了時間
+	elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count(); //処理に要した時間をnano秒に変換
+	std::cout << "duration = " << elapsed << "nanosec.\n";
+#ifdef VALUE_CHECK
+	std::cout << "KMatrix::reserve" << std::endl;
+	for (int i = 0; i < a_.row(); ++i)
+	{
+		for (int j = 0; j < a_.col(); ++j)
+		{
+			std::cout << a_(i, j) << ",";
+		}
+		std::cout << std::endl;
+	}
+#endif
 
 	return 0;
 }
